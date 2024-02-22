4c194eff7eeab98ab218e5d6aac6791c21f01dc0
srujithr <srujithrajan001@gmail.com>
2024-02-22 20:25:58 +0530

    good

--- qustion.js
+++ qustion.js
@@ -1,89 +1,89 @@
-let a = "apple,banana,cherry,manago";
-let b=a.length;
-console.log(b);
+// let a = "apple,banana,cherry,manago";
+// let b=a.length;
+// console.log(b);
 
 
-let a = "a for apple";
-let char=a.charAt(1);
-console.log(a);
-
-
-
-let c = "a man";
-let char = c.charCodeAt(0);
-console.log(c);
-
-let d = "good morning";
-let e =d.at(1);
-console.log(d);
-
-
-let e = "Apple, Banana, Kiwi";
-let a = e.slice(e);
-console.log(e);
-
-
-let p = "apple,orange,cherry";
-let g = p.substring(1,12);
-console.log(g);
-
-let m = "apple,orange,cherry";
-let k = m.substr(1,5);
-console.log(k);
-
-
-let num = "am happy ";
-let d = num.toUpperCase();
-console.log(d);
-
-let num = "HHHHHH";
-let d = num.toLowerCase();
-console.log(d);
-
-let sum = "am";
-let num = "boy";
-let d = sum.concat(" ",num);
-console.log(d);
-
-
-let d = "          softronics  ";
-let g = d.trim();
-console.log(g);
-
-
-let d = "          softronics  ";
-let g = d.trimEnd();
-console.log(g);
+// let a = "a for apple";
+// let char=a.charAt(1);
+// console.log(a);
 
 
 
-let d = "20";
-let g = d.padStart(5,"0");
-console.log(g);
+// let c = "a man";
+// let char = c.charCodeAt(0);
+// console.log(c);
+
+// let d = "good morning";
+// let e =d.at(1);
+// console.log(d);
 
 
-let d = "20";
-let g = d.padEnd(5,"X");
-console.log(g);
+// let e = "Apple, Banana, Kiwi";
+// let a = e.slice(e);
+// console.log(e);
 
 
-let d = "heloooo";
-let g = d.repeat(5);
-console.log(g);
+// let p = "apple,orange,cherry";
+// let g = p.substring(1,12);
+// console.log(g);
+
+// let m = "apple,orange,cherry";
+// let k = m.substr(1,5);
+// console.log(k);
+
+
+// let num = "am happy ";
+// let d = num.toUpperCase();
+// console.log(d);
+
+// let num = "HHHHHH";
+// let d = num.toLowerCase();
+// console.log(d);
+
+// let sum = "am";
+// let num = "boy";
+// let d = sum.concat(" ",num);
+// console.log(d);
+
+
+// let d = "          softronics  ";
+// let g = d.trim();
+// console.log(g);
+
+
+// let d = "          softronics  ";
+// let g = d.trimEnd();
+// console.log(g);
 
 
 
-let d = "heloooo";
-let g = d.replace("heloooo","heooo");
-console.log(g);
+// let d = "20";
+// let g = d.padStart(5,"0");
+// console.log(g);
 
 
-let d = "heloooo";
-let g = d.replaceAll("/heloooo/g","heooo");
-console.log(g);
+// let d = "20";
+// let g = d.padEnd(5,"X");
+// console.log(g);
 
-let d = "heloooo";
-let g = d.split("");
-console.log(g);
+
+// let d = "heloooo";
+// let g = d.repeat(5);
+// console.log(g);
+
+
+
+// let d = "heloooo";
+// let g = d.replace("heloooo","heooo");
+// console.log(g);
+
+
+// let d = "heloooo";
+// let g = d.replaceAll("/heloooo/g","heooo");
+// console.log(g);
+
+// let d = "heloooo";
+// let g = d.split("");
+// console.log(g);
 
 
---
+++ stringSearch.js
@@ -0,0 +1,36 @@
+// string indexof
+
+let a = "happy birthdayy 'to you' ";
+let b = a.indexOf('to you');
+console.log(b);
+
+
+let c = "happy birtday 'to youhh' me ";
+let d = c.lastIndexOf('to you');
+console.log(d);
+
+let e = "hello word 'happy' hi ";
+let f = e.search('happy');
+console.log(f);
+
+
+let text = "The rain in SPAIN stays mainly in the plain";
+let ts = text.match('ain');
+console.log(ts);
+
+let t = "The rain in SPAIN stays mainly in the plain";
+let p = t.matchAll('ain');
+console.log(p);
+
+let too = "The rain in SPAIN stays mainly in the plain";
+let o = too.includes('ain');
+console.log(too);
+
+let tos = "The rain in SPAIN stays mainly in the plain";
+let l = tos.startsWith('heloo');
+console.log(l);
+
+let to = "The rain in SPAIN stays mainly in the plain";
+let u = to.endsWith('heloo');
+console.log(u);
+
